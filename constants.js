86a323d79dc14160a8fff43048d2421ea32a92b1
Mark Foster <foster154@gmail.com>
2016-09-23 11:29:41 -0600

    env var troubleshooting

--- constants.js
+++ constants.js
@@ -1,3 +1,4 @@
 //export const API_URL = 'http://localhost:3000';
 //export const API_URL = 'http://photopay-api.herokuapp.com';
-export const API_URL = process.env.API_URL || 'http://localhost:3000';
+// export const API_URL = process.env.API_URL || 'http://localhost:3000';
+export const API_URL = NODE_ENV === "production" ? 'http://photopay-api.herokuapp.com' : 'http://localhost:3000';

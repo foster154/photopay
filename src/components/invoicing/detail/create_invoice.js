faa9eb2843dc2bf2a0aa7031a89d91635aca6bd0
Mark Foster <foster154@gmail.com>
2017-01-02 09:42:45 -0700

    fix linting errors

--- src/components/invoicing/detail/create_invoice.js
+++ src/components/invoicing/detail/create_invoice.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
-import { Field, reduxForm, formValueSelector } from 'redux-form';
-import { connect } from 'react-redux';
-import { createInvoice } from '../../../actions';
-import { fetchCustomers } from '../../../actions/customers';
-import Select from 'react-select';
-import CustomerSelect from './_customer_select';
-import { normalizeShareUrl } from './_normalize_share_url';
-require('../../../styles/invoicing/form.scss');
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { createInvoice } from '../../../actions'
+import { fetchCustomers } from '../../../actions/customers'
+import CustomerSelect from './_customer_select'
+import { normalizeShareUrl } from './_normalize_share_url'
+require('../../../styles/invoicing/form.scss')
 
 class CreateInvoice extends Component {
-  
-  componentWillMount() {
-    this.props.fetchCustomers();
+  componentWillMount () {
+    this.props.fetchCustomers()
   }
-  
-  handleFormSubmit(formProps) {
-    
-    const validShareUrl = normalizeShareUrl(formProps.shareUrl);
-    
+
+  handleFormSubmit (formProps) {
+    const validShareUrl = normalizeShareUrl(formProps.shareUrl)
+
     const expandedFormProps = {
       customerName: this.props.customerList[formProps.customer - 1].name,
       customerEmail: this.props.customerList[formProps.customer - 1].email,
@@ -27,41 +23,38 @@ class CreateInvoice extends Component {
       amount: formProps.amount,
       shareUrl: validShareUrl,
       displayShareLinkImmediately: formProps.displayShareLinkImmediately,
-      paid: formProps.paid,
-    };
-    console.info("expandedFormProps", expandedFormProps);
-    this.props.createInvoice(expandedFormProps);
+      paid: formProps.paid
+    }
+    console.info('expandedFormProps', expandedFormProps)
+    this.props.createInvoice(expandedFormProps)
   }
-  
-  renderAlert() {
+
+  renderAlert () {
     if (this.props.errorMessage) {
-      return <div className="msg-red">{this.props.errorMessage}</div>
+      return <div className='msg-red'>{this.props.errorMessage}</div>
     }
   }
-  
-  render() {
-    
-    const { hasCustomerValue, customerValue, handleSubmit } = this.props;
-      
-    let customerOptions = [ {} ];
-      
+
+  render () {
+    let customerOptions = [ {} ]
+
     if (this.props.customerList.length > 0) {
-      customerOptions = this.props.customerList.map(function(customer, index) {
+      customerOptions = this.props.customerList.map(function (customer, index) {
         return { value: index + 1, label: customer.name }
-      });
-      console.log("customerOptions", customerOptions);
+      })
+      console.log('customerOptions', customerOptions)
     }
-    
+
     return (
-      <div className="invoice-form-wrapper">
-        <h1 className="text-center">New Invoice</h1>
-        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+      <div className='invoice-form-wrapper'>
+        <h1 className='text-center'>New Invoice</h1>
+        <form onSubmit={this.props.handleSubmit(() => this.handleFormSubmit)}>
           {this.renderAlert()}
-          <div className="clearfix">
-            <fieldset className="customer-field">
-              <label htmlFor="customer">Customer:</label>
+          <div className='clearfix'>
+            <fieldset className='customer-field'>
+              <label htmlFor='customer'>Customer:</label>
               <Field
-                name="customer"
+                name='customer'
                 component={props =>
                   <CustomerSelect
                     {...props}
@@ -71,57 +64,65 @@ class CreateInvoice extends Component {
                 }
               />
             </fieldset>
-            <fieldset className="invoice-number-field">
-              <label htmlFor="invoiceNumber">Invoice #:</label>
-              <Field className="text-input" name="invoiceNumber" component="input" type="input" />
+            <fieldset className='invoice-number-field'>
+              <label htmlFor='invoiceNumber'>Invoice #:</label>
+              <Field className='text-input' name='invoiceNumber' component='input' type='input' />
             </fieldset>
           </div>
 
-          <h2 className="text-center">Items</h2>
-          <div className="clearfix">
-            <div className="clearfix">
-              <fieldset className="item-field">
+          <h2 className='text-center'>Items</h2>
+          <div className='clearfix'>
+            <div className='clearfix'>
+              <fieldset className='item-field'>
                 <label>Item:</label>
-                <Field className="text-input" name="item" component="input" type="input" />
+                <Field className='text-input' name='item' component='input' type='input' />
               </fieldset>
-              <fieldset className="amount-field">
+              <fieldset className='amount-field'>
                 <label>Amount:</label>
-                <Field className="text-input" name="amount" component="input" type="input" />
+                <Field className='text-input' name='amount' component='input' type='input' />
               </fieldset>
             </div>
-            <div className="clearfix">
-              <div className="total-label">Total:</div>
-              <div className="total-amount">${}</div>
+            <div className='clearfix'>
+              <div className='total-label'>Total:</div>
+              <div className='total-amount'>${}</div>
             </div>
           </div>
-          
-          <h2 className="text-center">After Payment Has Been Received</h2>
+
+          <h2 className='text-center'>After Payment Has Been Received</h2>
           <fieldset>
             <label>Share This Link:</label>
-            <Field className="text-input" name="shareUrl" component="input" type="input" />
+            <Field className='text-input' name='shareUrl' component='input' type='input' />
           </fieldset>
           <fieldset>
-            <Field name="displayShareLinkImmediately" component="input" type="checkbox"/>
+            <Field name='displayShareLinkImmediately' component='input' type='checkbox' />
             Display share link immediately (even before payment has been received)
             <br />
-            <Field name="paid" component="input" type="checkbox"/>
+            <Field name='paid' component='input' type='checkbox' />
             Mark invoice as paid
             <br />
           </fieldset>
 
-          <button className="save-btn clearfix" action="submit">Save Invoice</button>
+          <button className='save-btn clearfix' action='submit'>Save Invoice</button>
         </form>
       </div>
-    );
+    )
   }
 }
 
-function mapStateToProps(state) {
-  return { customerList: state.customers.customerList };
+function mapStateToProps (state) {
+  return { customerList: state.customers.customerList }
 }
 
 const form = reduxForm({
   form: 'createInvoice'
-});
+})
+
+CreateInvoice.propTypes = {
+  customerList: React.PropTypes.array,      // mapStateToProps
+  errorMessage: React.PropTypes.string,     // ?
+  handleSubmit: React.PropTypes.func,       // Redux form
+  fetchCustomers: React.PropTypes.func,     // Redux action creator
+  createInvoice: React.PropTypes.func       // Redux action creator
+}
 
 export default connect(mapStateToProps, { createInvoice, fetchCustomers })(form(CreateInvoice))
